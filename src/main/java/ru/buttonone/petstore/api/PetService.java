ac6a5a60bf64250fbc9691e718ada1f1c6385271
Tarasovdv <62928282+Tarasovdv@users.noreply.github.com>
2024-04-19 14:06:01 +0300

    [Jira-0002] tc-2 add logger (#2)
    * add test
    
    * add dependencies
    
    * modify
    
    * работа над ошибками
    
    * работа над ошибками BaseTest.java
    
    * форматирование кода
    
    * modify
    
    * modify
    
    * modify
    
    * modify
    add Endpoint.java
    
    * modify PetService.java
    
    * работа над ошибками
    
    * add logger
    
    * modify
    
    * modify
    
    * delete log4j2.xml
    
    ---------
    
    Co-authored-by: Tarasovdv <desant74268@yande.ru>

--- src/main/java/ru/buttonone/petstore/api/PetService.java
+++ src/main/java/ru/buttonone/petstore/api/PetService.java
@@ -2,15 +2,18 @@ package ru.buttonone.petstore.api;
 
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
+import lombok.extern.log4j.Log4j2;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static ru.buttonone.petstore.api.Endpoint.*;
 import static ru.buttonone.petstore.spec.Spec.*;
 
+@Log4j2
 public class PetService {
 
-    private boolean checkPetExistById(long petId) {
+    public boolean checkPetExistById(long petId) {
+        log.info("Проверка существование питомца по ID = {}", petId);
         Response response = given()
                 .spec(requestSpec())
                 .pathParam("petId", petId)
@@ -20,11 +23,13 @@ public class PetService {
                 .then()
                 .log()
                 .status();
+
         return response.getStatusCode() == 200;
     }
 
     @Step("Предоставление питомца по id = {id}")
     public PetService findPetById(long petId) {
+        log.info("Предоставление питомца по ID = {}", petId);
         given()
                 .spec(requestSpec())
                 .pathParam("petId", petId)
@@ -32,11 +37,13 @@ public class PetService {
                 .get(PET_BY_ID)
                 .then()
                 .spec(responseSpec());
+
         return this;
     }
 
     @Step("Удаление питомца по id = {petId}")
     public void deletePetById(long petId) {
+        log.info("Удаление питомца по ID = {}", petId);
         if (checkPetExistById(petId)) {
             given()
                     .spec(requestSpec())
@@ -45,11 +52,16 @@ public class PetService {
                     .delete(PET_BY_ID)
                     .then()
                     .spec(responseSpec());
+
+        } else {
+            log.error("Питомца НЕ существует -> ID = {}", petId);
+            throw new RuntimeException("Питомца с ID = " + petId + " НЕ существует");
         }
     }
 
     @Step("Предоставление всех питомцев со статусом = {status}")
     public PetService findPetByStatus(String status) {
+        log.info("Предоставление всех питомцев со статусом = {}", status);
         given()
                 .spec(requestSpec())
                 .queryParam("status", status)
@@ -57,12 +69,18 @@ public class PetService {
                 .get(PET_BY_STATUS)
                 .then()
                 .spec(responseSpec());
+
         return this;
     }
 
     @Step("Добавление нового питомца")
-    public PetService addNewPet(long id, String petJson) {
-        if (checkPetExistById(id)) throw new RuntimeException("Питомец с id = " + id + " уже существует");
+    public PetService addNewPet(long petId, String petJson) {
+        log.info("Добавление нового питомца с ID = {}", petId);
+        if (checkPetExistById(petId)) {
+            log.error("Питомец уже существует -> ID = {}", petId);
+            throw new RuntimeException("Питомец с ID = " + petId + " уже существует");
+        }
+
         given()
                 .spec(requestSpec())
                 .contentType(JSON)
@@ -71,11 +89,13 @@ public class PetService {
                 .post(PET)
                 .then()
                 .spec(responseSpec());
+
         return this;
     }
 
     @Step("Изменение имени на {name} и статуса питомца на {status} через id = {petId}")
     public PetService partialUpdatePet(long petId, String name, String status) {
+        log.info("Изменение имени на {} и статуса питомца на {} через ID = {}", name, status, petId);
         given()
                 .spec(requestSpec())
                 .pathParam("petId", petId)
@@ -85,11 +105,13 @@ public class PetService {
                 .post(PET_BY_ID)
                 .then()
                 .spec(responseSpec());
+
         return this;
     }
 
     @Step("Полное изменение данных о питомце")
     public PetService fullUpdatePet(String petJson) {
+        log.info("Полное изменение данных о питомце");
         given()
                 .spec(requestSpec())
                 .contentType(JSON)
@@ -98,19 +120,7 @@ public class PetService {
                 .put(PET)
                 .then()
                 .spec(responseSpec());
-        return this;
-    }
 
-    @Step("Проверка отсутствия данных о питомце с id = {petId} по запросу")
-    public PetService checkNoDataAboutPet(long petId) {
-        given()
-                .spec(requestSpec())
-                .pathParam("petId", petId)
-                .when()
-                .get(PET_BY_ID)
-                .then()
-                .log().status()
-                .statusCode(404);
         return this;
     }
 }
